8a551080dabcef29c68c0933f44902d11132be9a
Chaitanya-14 <bchaitanya1407@gmail.com>
2026-03-17 20:06:38 +0100

    Vending machine

---
+++ Labs_and_Assignments/lab6_ass6/assignments/ass2/RTL/vending_machine.v
@@ -0,0 +1,67 @@
+module vending_machine(clk,rst,co_in0,co_in1,x_out,y_out);
+    input clk,rst,co_in0,co_in1;
+    output reg x_out,y_out;
+
+    reg [1:0] present_state, next_state;
+
+    parameter S0 = 2'b00;
+    parameter S1 = 2'b01;
+    parameter S2 = 2'b10;
+
+    // sequential for present state determination
+    always @ (posedge clk)
+        begin
+            if (rst)
+                present_state <= S0;
+            else 
+                present_state <= next_state;        
+        end
+
+    // Next state combinational logic
+    always @ (present_state,co_in0,co_in1)
+        next_state = S0;
+        x_out = 1'b0;
+        y_out = 1'b0;
+        begin
+            case(present_state)
+                S0 : if (co_in0 == 1'b0) begin
+                        next_state = S0;
+                        x_out = 0 ; y_out = 0; 
+                        end
+                    else if (co_in0 == 1'b1 && co_in1 == 1'b0) begin
+                        next_state = S1;
+                        x_out = 0 ; y_out = 0; 
+                    end
+                    else if (co_in0 == 1'b1 && co_in1 == 1'b1) begin
+                        next_state = S2;
+                        x_out = 0 ; y_out = 0;
+                    end
+                S1 : if (co_in0 == 1'b0) begin
+                        next_state = S1;
+                        x_out = 0 ; y_out = 0; 
+                        end
+                    else if (co_in0 == 1'b1 && co_in1 == 1'b0) begin
+                        next_state = S2;
+                        x_out = 0 ; y_out = 0; 
+                    end
+                    else if (co_in0 == 1'b1 && co_in1 == 1'b1) begin
+                        next_state = S0;
+                        x_out = 1 ; y_out = 0;
+                    end
+                S2 : if (co_in0 == 1'b0) begin
+                        next_state = S2;
+                        x_out = 0 ; y_out = 0; 
+                        end
+                    else if (co_in0 == 1'b1 && co_in1 == 1'b0) begin
+                        next_state = S0;
+                        x_out = 1 ; y_out = 0; 
+                    end
+                    else if (co_in0 == 1'b1 && co_in1 == 1'b1) begin
+                        next_state = S0;
+                        x_out = 1 ; y_out = 1;
+                    end
+
+            endcase
+        end
+
+endmodule
