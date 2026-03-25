86bcfc49f259ae10af0217a2b674570e68a9bbca
Chaitanya-14 <bchaitanya1407@gmail.com>
2026-03-25 12:06:26 +0100

    soft reset logic update synchroniser

--- Mini_projects/Router_3X1_project/rtl/router_sync.v
+++ Mini_projects/Router_3X1_project/rtl/router_sync.v
@@ -18,9 +18,9 @@ module router_sync # (parameter FIFO1 = 3'b001  FIFO2 = 3'b010  FIFO3 = 3'b100 )
                     output valid_out_0,
                     output valid_out_1,
                     output valid_out_2,
-                    output soft_reset_0,
-                    output soft_reset_1,
-                    output soft_reset_2, );
+                    output reg soft_reset_0,
+                    output reg soft_reset_1,
+                    output reg soft_reset_2, );
 
 // internal register
 reg [1:0] addr;
@@ -53,10 +53,113 @@ always @ (*)
     end
 
 
-// valid out logic
-assign valid_out_0 = ~empty;
-assign valid_out_1 = ~empty;
-assign valid_out_2 = ~empty;
+// valid out logic - output data will be valid at channels when valid_out is high - it is high when the fifo is not empty
+assign valid_out_0 = ~empty_0; // comes from FIFO 0 , 1 and 2 respectively.
+assign valid_out_1 = ~empty_1;
+assign valid_out_2 = ~empty_2;
+
+// soft reset 0
+always @ (posedge clock)
+    begin
+        if (~resetn)
+            begin
+                count_read_0 <= 0;
+                soft_reset_0 <= 0;
+            end
+        else if (~valid_out_0) // represents FIFO is empty
+            begin
+                count_read_0 <= 0;
+                soft_reset_0 <= 0;
+            end
+        else if (read_enb_0) // read is enabled and counter need to be initialised to zero, need not to be counting forward
+            begin
+                count_read_0 <=0;
+                soft_reset <= 0;
+            end
+        else // no reading happening, or valid out might be high
+            begin
+                if (count_read_0 == 29)
+                    begin
+                        count_read_0 <= 0;
+                        soft_reset_0 <= 1;
+                    end
+                else 
+                    begin
+                        count_read_0 <= count_read_0 +  1;
+                        soft_reset <= 0; 
+                    end
+            end
+    end
+
+// soft reset logic 1
+
+always @ (*)
+    begin
+        if (resetn)
+            begin
+                count_read_1 <= 0;
+                soft_reset <= 0;
+            end
+        else if (~valid_out_1)
+            begin
+                count_read_1 <= 0;
+                soft_reset <= 0;
+            end
+        else if (read_enb_1)
+            begin
+                count_read_2 <= 0;
+                soft_reset <= 0;
+            end
+        else begin
+            if (count_read_1 == 29)
+                begin
+                    count_read_1 <=0;
+                    soft_reset <=1;
+                end
+                else 
+                    begin
+                        count_read_1 <= count_read_1 + 1;
+                        soft_reset <= 0;
+                    end
+        end
+        
+
+    end
+
+    
+always @ (*)
+    begin
+        if (resetn)
+            begin
+                count_read_1 <= 0;
+                soft_reset <= 0;
+            end
+        else if (~valid_out_1)
+            begin
+                count_read_1 <= 0;
+                soft_reset <= 0;
+            end
+        else if (read_enb_1)
+            begin
+                count_read_2 <= 0;
+                soft_reset <= 0;
+            end
+        else begin
+            if (count_read_1 == 29)
+                begin
+                    count_read_1 <=0;
+                    soft_reset <=1;
+                end
+                else 
+                    begin
+                        count_read_1 <= count_read_1 + 1;
+                        soft_reset <= 0;
+                    end
+        end
+        
+
+    end
+
 
 
 
